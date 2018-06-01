2260b1f67e6b71f663e2e4d92571fb99d31769ca
admin <admin@DESKTOP>
2018-06-01 15:47:37 +0200

    Add project files.

---
+++ OnvifLogInfoTester/MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnvifVideoSample
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+
+			InitializeComponent();
+			address.GotFocus += InitTextbox;
+			user.GotFocus += InitTextbox;
+			password.GotFocus += InitTextbox;
+			button.Click += OnConnect;
+		}
+
+        private void TestThis()
+        {
+            // 
+            //or "/onvif_device"));
+            var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+            device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+            device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+            device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+
+
+            var services = device.GetServices(false);
+
+            string Model = "";
+            string FirmwareVersion = "";
+            string SerialNumber = "";
+            string HardwareId = "";
+            string returnDeviceInformation = "";
+
+            try
+            {
+                returnDeviceInformation = device.GetDeviceInformation(out Model, out FirmwareVersion, out SerialNumber, out HardwareId);
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetDeviceInformation throw an exception:");
+                listBox.Items.Add(ex.Message);
+            }
+
+
+            //OnvifVideoSample.device.RelayOutput[] outputnum = device.GetRelayOutputs();
+
+            listBox.Items.Add("Device under test: " + returnDeviceInformation);
+            listBox.Items.Add("Model: " + Model);
+            listBox.Items.Add("FW: " + FirmwareVersion);
+            listBox.Items.Add("SerialNumber: " + SerialNumber);
+            listBox.Items.Add("");
+
+            listBox.Items.Add("Result for GetSystemLog with \"Access\"");
+
+            try
+            {
+                device.SystemLog Log = device.GetSystemLog(OnvifVideoSample.device.SystemLogType.Access); //OnvifVideoSample.device.SystemLogType.Access
+                string output = "Log-String: " + Log.String;
+                listBox.Items.Add(output);
+                if (Log.Binary == null)
+                    listBox.Items.Add("Log-Binary was NULL");
+                else
+                {
+                    output = "Log-Binary" + Log.Binary;
+                    listBox.Items.Add(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetSystemLog with \"Access\" did throw an exception:");
+                listBox.Items.Add(ex.Message);
+            }
+
+            listBox.Items.Add("");
+            listBox.Items.Add("");
+            listBox.Items.Add("Result for GetSystemLog with \"System\"");
+
+            try
+            {
+                device.SystemLog Log = device.GetSystemLog(OnvifVideoSample.device.SystemLogType.System); //OnvifVideoSample.device.SystemLogType.Access
+                string output = "Log-String: " + Log.String;
+                listBox.Items.Add(output);
+                if (Log.Binary == null)
+                    listBox.Items.Add("Log-Binary was NULL");
+                else
+                {
+                    output = "Log-Binary" + Log.Binary;
+                    listBox.Items.Add(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetSystemLog with \"System\" did throw an exception: ");
+                listBox.Items.Add(ex.Message);
+            }
+
+
+            listBox.Items.Add("");
+            listBox.Items.Add("");
+            listBox.Items.Add("GetSystemSupportInformation TEST: ");
+            try
+            {
+                OnvifVideoSample.device.SupportInformation supportInfo = device.GetSystemSupportInformation();
+                listBox.Items.Add("SupportInformation got string: " + supportInfo.String);
+
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetSystemSupportInformation did throw an exception: ");
+                listBox.Items.Add(ex.Message);
+            }
+
+
+            listBox.Items.Add("");
+            listBox.Items.Add("");
+            listBox.Items.Add("GetSystemBackup TEST: ");
+            try
+            {
+                OnvifVideoSample.device.BackupFile[] backupFile = device.GetSystemBackup();
+                listBox.Items.Add("BackupFile did return.");
+
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetSystemBackup did throw an exception: ");
+                listBox.Items.Add(ex.Message);
+            }
+
+
+
+
+            listBox.Items.Add("");
+            listBox.Items.Add("");
+            listBox.Items.Add("GetSystemUris TEST: ");
+            try
+            {
+                string SupportInfoUri;
+                string SystemBackupUri;
+                OnvifVideoSample.device.GetSystemUrisResponseExtension Extension;
+                OnvifVideoSample.device.SystemLogUri[] systemUris = device.GetSystemUris(out SupportInfoUri, out SystemBackupUri, out Extension);
+
+                listBox.Items.Add("systemUris: " + systemUris);
+                listBox.Items.Add("SupportInfoUri: " + SupportInfoUri);
+                listBox.Items.Add("SystemBackupUri: " + SystemBackupUri);
+                listBox.Items.Add("Extension: " + Extension);
+
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Add("GetSystemUris did throw an exception: ");
+                listBox.Items.Add(ex.Message);
+            }
+            int b = 0;
+        }
+
+        private void OnConnect(object sender, RoutedEventArgs e)
+		{
+
+            if (string.IsNullOrEmpty(address.Text))
+            {
+                //Bosch
+                address.Text = "193.159.244.134";
+                user.Text = "service";
+                password.Password = "Xbks8tr8vT!";
+                TestThis();
+
+                //Bosch
+                address.Text = "193.159.244.132";
+                user.Text = "service";
+                password.Password = "Xbks8tr8vT!";
+                TestThis();
+
+                //Axis
+                address.Text = "195.60.68.239";
+                user.Text = "operator";
+                password.Password = "Onv!f2018";
+                TestThis();
+
+                //Hikvision
+                address.Text = "123.157.208.28:81";
+                user.Text = "admin";
+                password.Password = "abcd1234";
+                TestThis();
+
+                //Uniview
+                address.Text = "61.164.52.166:88";
+                user.Text = "admin";
+                password.Password = "Uniview2018";
+                TestThis();
+
+                //Dahua
+                address.Text = "60.191.94.122:8086";
+                user.Text = "admin";
+                password.Password = "a1b2c3d4";
+                TestThis();
+            }
+            else
+            {
+                TestThis();
+            }
+            
+        }
+
+		private void InitTextbox(object sender, RoutedEventArgs e)
+		{
+			if (((sender as Control).Foreground as SolidColorBrush)?.Color == Colors.DarkGray) {
+				if (sender is TextBox) {
+					(sender as TextBox).Text = "";
+				}
+				else if (sender is PasswordBox) {
+					(sender as PasswordBox).Password = "";
+				}
+				(sender as Control).Foreground = new SolidColorBrush(Colors.Black);
+			}
+		}
+
+
+		System.ServiceModel.Channels.Binding WsdlBinding
+		{
+			get
+			{
+				HttpTransportBindingElement httpTransport = new HttpTransportBindingElement();
+				httpTransport.AuthenticationScheme = System.Net.AuthenticationSchemes.Digest;
+				return new CustomBinding(new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, Encoding.UTF8), httpTransport);
+			}
+		}
+
+	}
+}
