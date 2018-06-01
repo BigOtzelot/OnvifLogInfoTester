3c3faf8aa7835a1187cb1e7586f912bd7b4fe770
Thomas Oetzel <otzelot@web.de>
2018-06-01 23:43:44 +0200

    Pictures and pcap-files.

--- OnvifLogInfoTester/MainWindow.xaml.cs
+++ OnvifLogInfoTester/MainWindow.xaml.cs
@@ -37,13 +37,22 @@ namespace OnvifVideoSample
         {
             // 
             //or "/onvif_device"));
-            var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
-            device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
-            device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
-            device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+            try
+            {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+                var services = device.GetServices(false);
+            }
+            catch(Exception ex)
+            {
+                listBox.Items.Add(address.Text);
+                listBox.Items.Add("GetServices throw an exception:");
+                listBox.Items.Add(ex.Message);
+            }
 
-
-            var services = device.GetServices(false);
+            
 
             string Model = "";
             string FirmwareVersion = "";
@@ -53,6 +62,10 @@ namespace OnvifVideoSample
 
             try
             {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 returnDeviceInformation = device.GetDeviceInformation(out Model, out FirmwareVersion, out SerialNumber, out HardwareId);
             }
             catch (Exception ex)
@@ -75,6 +88,10 @@ namespace OnvifVideoSample
 
             try
             {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 device.SystemLog Log = device.GetSystemLog(OnvifVideoSample.device.SystemLogType.Access); //OnvifVideoSample.device.SystemLogType.Access
                 string output = "Log-String: " + Log.String;
                 listBox.Items.Add(output);
@@ -98,6 +115,10 @@ namespace OnvifVideoSample
 
             try
             {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 device.SystemLog Log = device.GetSystemLog(OnvifVideoSample.device.SystemLogType.System); //OnvifVideoSample.device.SystemLogType.Access
                 string output = "Log-String: " + Log.String;
                 listBox.Items.Add(output);
@@ -121,6 +142,10 @@ namespace OnvifVideoSample
             listBox.Items.Add("GetSystemSupportInformation TEST: ");
             try
             {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 OnvifVideoSample.device.SupportInformation supportInfo = device.GetSystemSupportInformation();
                 listBox.Items.Add("SupportInformation got string: " + supportInfo.String);
 
@@ -137,6 +162,10 @@ namespace OnvifVideoSample
             listBox.Items.Add("GetSystemBackup TEST: ");
             try
             {
+                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 OnvifVideoSample.device.BackupFile[] backupFile = device.GetSystemBackup();
                 listBox.Items.Add("BackupFile did return.");
 
@@ -157,6 +186,10 @@ namespace OnvifVideoSample
             {
                 string SupportInfoUri;
                 string SystemBackupUri;
+                                var device = new device.DeviceClient(WsdlBinding, new EndpointAddress("http://" + address.Text + "/onvif/device_service"));
+                device.ClientCredentials.HttpDigest.ClientCredential.UserName = user.Text;
+                device.ClientCredentials.HttpDigest.ClientCredential.Password = password.Password;
+                device.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
                 OnvifVideoSample.device.GetSystemUrisResponseExtension Extension;
                 OnvifVideoSample.device.SystemLogUri[] systemUris = device.GetSystemUris(out SupportInfoUri, out SystemBackupUri, out Extension);
 
